e0470feaf8affa61b9efc40fcf76068c3f8097e4
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-21 13:24:35 +0100

    Calculator operators

--- js/script.js
+++ js/script.js
@@ -2,12 +2,42 @@ class Calculator{
     constructor(previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement=previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+    
+    clear(){
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+    chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+    compute(){
+        
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText =this.previousOperand;
     }
 }
 
 
 
-const number = document.querySelectorAll('[data-number]');
+const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelectorAll('[data-equals]');
 const deleteButton = document.querySelectorAll('[data-delete]');
@@ -15,3 +45,18 @@ const allClearButton = document.querySelectorAll('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button =>{
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+});
+
+operationButtons.forEach(button =>{
+    button.addEventListener('click',() => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+});
